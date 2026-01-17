fa6de0d927f26a2f0d434fa36851c445c04a163f
Avishkar jadhav <avishkarjadhav24@gmail.com>
2026-01-17 17:04:05 +0530

    Improve voice prompt and update README

--- python-voice-to-text/main.py
+++ python-voice-to-text/main.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    print("..Initialised..")
+    print("Listening... Please speak clearly for 5 seconds")
     audio = r.record(source, duration=5)
     text = r.recognize_google(audio)
     print(text)
